00e88b9785c51e33819fcec4deeec7a2a0ccc5b0
Zaharchenko Anton <106864247+Black-Catt@users.noreply.github.com>
2023-02-27 16:50:10 +0200

    Update SpecificBook.jsx

--- src/components/SpecificBook/SpecificBook.jsx
+++ src/components/SpecificBook/SpecificBook.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import Header from '../Header/Header';
@@ -39,7 +39,6 @@ const SpecificBook = () => {
         <div data-testid="specific-book" className="book">
           <div className="book__container">
             <button
-              href=""
               className="back-btn"
               onClick={() => navigate(-1)}
               to="book-list"
